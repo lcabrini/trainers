dc8e07c8b4915216e91db7ba99dfb6b4ae62cf51
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-20 12:27:48 +0000

    Serve files in the resources directory.

--- main.go
+++ main.go
@@ -8,5 +8,8 @@ func main() {
     auth := &Auth{}
     auth.Setup()
 
+    fs := http.FileServer(http.Dir("/resources/"))
+    http.Handle("/resources/", fs)
+
     http.ListenAndServe(":8000", nil)
 }
